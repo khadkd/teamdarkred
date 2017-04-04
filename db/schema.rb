ff79fbed1684bd1903fbd94004344fbd04ad40e4
Dipesh Khadka <dipesh.khadka@mnsu.edu>
2017-04-04 21:08:22 +0000

    Fluid-containers bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 20170324054825) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_twitters", force: :cascade do |t|
-    t.string   "name"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
